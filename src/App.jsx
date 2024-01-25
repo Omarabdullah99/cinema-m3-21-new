c29a1ad11df006fe9c4c058ecf22b6fa860cc703
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-01-25 18:51:53 +0600

    MovieRateing&Footer---HtmltoReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import MovieList from "./cine/MovieList";
@@ -7,11 +8,12 @@ export default function App() {
     <div>
       <Header />
       <main>
-        <div className=" grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
           <MovieList />
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 
+import MovieRating from "./MovieRating";
+
 export default function MovieCard({ movie }) {
   return (
     <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
@@ -8,11 +10,7 @@ export default function MovieCard({ movie }) {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <img src="star.svg" width="14" height="14" alt="" />
-          <img src="star.svg" width="14" height="14" alt="" />
-          <img src="star.svg" width="14" height="14" alt="" />
-          <img src="star.svg" width="14" height="14" alt="" />
-          <img src="star.svg" width="14" height="14" alt="" />
+        <MovieRating  value={movie.rating}/>
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
