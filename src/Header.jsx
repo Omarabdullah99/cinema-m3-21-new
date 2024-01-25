f9f0a14a955951253c6f0209c991a0a58b3517f1
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-01-25 22:12:38 +0600

    Header->shopiciconclick-AddToCartshow-html--react

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,52 @@
+import { useState } from "react";
+import CartDetailModel from "./cine/CartDetailModel";
 
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  const handleShowCart=()=>{
+    setShowCart(true)
+  }
   return (
     <header>
-    <nav className="container flex items-center justify-between space-x-10 py-6">
+      {showCart && <CartDetailModel onClose={()=>setShowCart(false)}/>}
+      <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
-            <img src="logo.svg" width="139" height="26" alt="" />
+          <img src="logo.svg" width="139" height="26" alt="" />
         </a>
 
         <ul className="flex items-center space-x-5">
-            <li>
-                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                    <img src="ring.svg" width="24" height="24" alt="" />
-                </a>
-            </li>
-            <li>
-                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                    <img src="../public/icons/moon.svg" width="24" height="24" alt="" />
-                </a>
-            </li>
-            <li>
-                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                    <img src="shopping-cart.svg" width="24" height="24" alt="" />
-                </a>
-            </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src="ring.svg" width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img
+                src="../public/icons/moon.svg"
+                width="24"
+                height="24"
+                alt=""
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+              onClick={handleShowCart}
+            >
+              <img src="shopping-cart.svg" width="24" height="24" alt="" />
+            </a>
+          </li>
         </ul>
-    </nav>
-</header>
-  )
+      </nav>
+    </header>
+  );
 }
--- src/cine/MovieDetailsModal.jsx
+++ src/cine/MovieDetailsModal.jsx
@@ -28,7 +28,7 @@ export default function MovieDetailsModal({ movie,setShowmodal }) {
                 className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                 href="#"
               >
-                <img src="./assets/tag.svg" alt="" />
+                <img src="tag.svg" alt="" />
                 <span>${movie.price} | Add to Cart</span>
               </a>
               <a
