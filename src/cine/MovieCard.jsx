2d434aeac37e78ff0d682d7566ed9fb7f0d2dde6
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-01-25 19:12:54 +0600

    MoviedetaiModa-htme--react-modal-open&of

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,25 +1,34 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+import MovieDetailsModal from "./MovieDetailsModal";
 import MovieRating from "./MovieRating";
 
 export default function MovieCard({ movie }) {
+  const [showModal, setShowmodal] = useState(false);
+  function handleShowModal(){
+    setShowmodal(true)
+  }
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img className="w-full object-cover" src={movie.cover} alt="" />
-      <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{movie.title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-        <div className="flex items-center space-x-1 mb-5">
-        <MovieRating  value={movie.rating}/>
-        </div>
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <img src="tag.svg" alt="" />
-          <span>${movie.price} | Add to Cart</span>
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {showModal && <MovieDetailsModal movie={movie} setShowmodal={setShowmodal}/>}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl" onClick={handleShowModal}>
+        <img className="w-full object-cover" src={movie.cover} alt="" />
+        <figcaption className="pt-4">
+          <h3 className="text-xl mb-1">{movie.title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+          <div className="flex items-center space-x-1 mb-5">
+            <MovieRating value={movie.rating} />
+          </div>
+          <a
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+          >
+            <img src="tag.svg" alt="" />
+            <span>${movie.price} | Add to Cart</span>
+          </a>
+        </figcaption>
+      </figure>
+    </>
   );
 }
