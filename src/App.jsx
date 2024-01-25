30f13e511c2b05f49634a3adc66dba2c4269dcba
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-01-25 15:36:22 +0600

    HeaderHtml----Reactconvert

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./Header";
 
 
 export default function App() {
   return (
     <div>
-    <h1 className="text-6xl text-red-500 text-center">Hello</h1>
+    <Header />
     </div>
   )
 }
