d27262202ffcc8da663defc3db5be873fc560331
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-01-25 17:07:07 +0600

    movielist---html--react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Header from "./Header";
-
+import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
 
 export default function App() {
   return (
     <div>
-    <Header />
+      <Header />
+      <main>
+        <div className=" grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
